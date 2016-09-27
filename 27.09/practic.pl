f2f26bc9b77a3ceb0b6844dd5b7c843f172b3595
Ivan Maslintsyn <maslintsyn@gmail.com>
2016-09-27 12:00:41 +0500

    27.09

--- 27.09/practic.pl
+++ 27.09/practic.pl
@@ -1,27 +1,27 @@
 :- initialization(consult('kinship.pro')).
 
-pred(X,Y) :- parent(X,Y).
+pred(X,Y) :- (var(Y);var(X)), parent(X,Y).
+pred(X,Y) :- nonvar(X), nonvar(Y), parent(X,Y),!.
 pred(X,Y) :- parent(X,Z), pred(Z,Y).
 
-brother_1(X,Y) :- male(X), parent(P,X), parent(P,Y), \+ X = Y.
+b_or_s_1(X,Y) :- parent(P,X), parent(P,Y),parent(P1,X), parent(P1,Y), \+ X = Y.
+b_or_s(X,Y) :- setof(Y, b_or_s_1(X,Y), Lst), nth(_,Lst,Y).
+brother_1(X,Y) :- male(X), parent(P,X), parent(P,Y),parent(P1,X), parent(P1,Y), \+ P1 = P, \+ X = Y.
 
 married_1(X,Y) :- parent(X,Ch), parent(Y,Ch), \+ X = Y.
 husband_1(X,Y) :- male(X), married(X,Y).
 
-brother(X,Y) :- findall(X, brother_1(X,Y), L), set(L,Lst), nth(_,Lst,X).
+brother(X,Y) :- setof(X, brother_1(X,Y), Lst), nth(_,Lst,X).
 
-married(X,Y) :- findall(X, married_1(X,Y), L), set(L,Lst), nth(_,Lst,X).
+married(X,Y) :- setof(X, married_1(X,Y), Lst), nth(_,Lst,X).
 
-husband(X,Y) :- findall(X, husband_1(X,Y), L), set(L,Lst), nth(_,Lst,X).
+husband(X,Y) :- setof(X, husband_1(X,Y), Lst), nth(_,Lst,X).
 
-num_of_children(X,N):- findall(Ch, parent(X,Ch), L), set(L,Lst), length(Lst,N).
+num_of_children(X,N):- setof(Ch, parent(X,Ch), Lst), length(Lst,N).
 
-subtract([], _, []) :- !.
-subtract([A|C], B, D) :-
-        memberchk(A, B), !,
-        subtract(C, B, D).
-subtract([A|B], C, [A|D]) :-
-        subtract(B, C, D).
+cousin(X,Y) :- parent(F,X), nephews(Lst,F), nth(_,Lst,Y). 
 
-set([], []).
-set([H|T], [H|T1]) :- subtract(T, [H], T2), set(T2, T1).
+nephews(Lst,Man) :- findall(Ch,(parent(X,Ch),b_or_s(X,Man)),L), sort(L,Lst).
+
+famiy(L) :- L = [Father,Mother|Ls],
+	setof(Ch,(parent(Father,Ch),parent(Mother,Ch)), Lst), Ls = Lst.
