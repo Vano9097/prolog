262413ef5af88d1f43a3913d1cd4fd78f8d875d8
Ivan Maslintsyn <maslintsyn@gmail.com>
2016-09-27 09:59:05 +0500

    up

--- 27.09/practic.pl
+++ 27.09/practic.pl
@@ -1,8 +1,9 @@
 :- initialization(consult('kinship.pro')).
 
+pred(X,Y) :- parent(X,Y).
+pred(X,Y) :- parent(X,Z), pred(Z,Y).
+
 brother_1(X,Y) :- male(X), parent(P,X), parent(P,Y), \+ X = Y.
-%brother(X,Y) :- male(X), parent(P1,X), parent(P1,Y), parent(P2,X), parent(P2,Y),
-%	 female(P1),male(P2), \+ X = Y.
 
 married_1(X,Y) :- parent(X,Ch), parent(Y,Ch), \+ X = Y.
 husband_1(X,Y) :- male(X), married(X,Y).
@@ -14,6 +15,13 @@ married(X,Y) :- findall(X, married_1(X,Y), L), set(L,Lst), nth(_,Lst,X).
 husband(X,Y) :- findall(X, husband_1(X,Y), L), set(L,Lst), nth(_,Lst,X).
 
 num_of_children(X,N):- findall(Ch, parent(X,Ch), L), set(L,Lst), length(Lst,N).
+
+subtract([], _, []) :- !.
+subtract([A|C], B, D) :-
+        memberchk(A, B), !,
+        subtract(C, B, D).
+subtract([A|B], C, [A|D]) :-
+        subtract(B, C, D).
 
 set([], []).
 set([H|T], [H|T1]) :- subtract(T, [H], T2), set(T2, T1).
